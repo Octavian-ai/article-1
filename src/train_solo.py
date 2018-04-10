0383425efaa9df62a00f68489b121d7296f88b59
Andrew Jefferson <andrew.jefferson@neotechnology.com>
2018-04-10 22:08:13 +0200

    read the docs for evaluate_and_train

--- src/train_solo.py
+++ src/train_solo.py
@@ -23,6 +23,8 @@ def train(args):
 	suffix = f"{len(person_ids)}-{len(product_ids)}-{args.embedding_width}"
 	output_dir = args.output_dir + suffix
 
+	os.makedirs(output_dir, exist_ok=True)
+
 	estimator = tf.estimator.Estimator(
 		model_fn=model_fn,
 		model_dir=output_dir,
@@ -34,21 +36,28 @@ def train(args):
 		})
 
 	combined_train = True
-	max_steps = round(args.data_passes_per_epoch * len(data_train) / args.batch_size)
 
 	if combined_train:
+
+		max_steps = round(
+			float(args.data_passes_per_epoch * len(data_train) * args.epochs) / args.batch_size
+		)
+
 		# Specs for train and eval
 		train_spec = tf.estimator.TrainSpec(input_fn=data_train.input_fn, max_steps=max_steps)
 		eval_spec = tf.estimator.EvalSpec(input_fn=data_test.input_fn, throttle_secs=10)
 
-		for i in range(args.epochs):
-			tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+		tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
 
 	else:
 
+		max_steps = round(
+			float(args.data_passes_per_epoch * len(data_train)) / args.batch_size
+		)
+
 		for i in range(args.epochs):
 			estimator.train(
-				input_fn=data_train.input_fn, 
+				input_fn=data_train.input_fn,
 				max_steps=max_steps
 			)
 
@@ -57,10 +66,8 @@ def train(args):
 	)
 
 
-
 	print(result)
 	print(f"Accuracy: {round(result['accuracy']*100)}%")
-
 
 
 
